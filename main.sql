e40faccffa7a606964442fac71ca85d42340e1d8
tryspidy <68438592+tryspidy@users.noreply.github.com>
2021-10-13 03:58:57 +0530

    Initial commit

---
+++ main.sql
@@ -0,0 +1,6 @@
+Mit DELETE werden einzelne oder mehrere Datensätze gelöscht. Mit WHERE 
+werden nur bestimmte Datensätez gelöscht. 
+
+  DELETE FROM suppliers
+  WHERE 
+      supplier_name = 'Microsoft';
